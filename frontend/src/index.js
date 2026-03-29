08b4bfa4f8938e604be00d649a14e0f606c1275b
huijiecai <huijiecai@deeproute.ai>
2026-03-29 15:38:25 +0800

    fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/reset.css';
 import './index.css';
 import App from './App';
-import 'antd/dist/reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
